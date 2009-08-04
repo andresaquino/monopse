a1b96e04f28d85bc6329ed3650c74b8f37bae8a1
Andres Aquino <cesar.aquino@nextel.com.mx>
2009-08-03 19:12:33 -0500

    ejecutar y poder migrar configuraciones

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/sh 
-# vim: set ts=2 sw=2 sts=2 et si ai: 
-
-# install.sh -- put here a short description 
-# ----------------------------------------------------------------------------
-# (c) 2009 Nextel de México S.A. de C.V.
-# Andrés Aquino Morales <andres.aquino@gmail.com>
-# All rights reserved.
+# vim: set ts=3 sw=3 sts=3 et si ai: 
 # 
+# install.sh -- instalar monopse en el directorio
+# --------------------------------------------------------------------
+# (c) 2008 NEXTEL DE MEXICO
+# 
+# César Andrés Aquino <cesar.aquino@nextel.com.mx>
 
 mkdir -p ~/bin
+# si existe, moverlo
+[ -d ~/monopse ] && mv ~/monopse ~/monopse.old
 [ -d ~/monopse.git ] && mv ~/monopse.git ~/monopse
+[ -d ~/monopse.old ] && cp -rp ~/monopse.old/*-monopse.conf ~/monopse/setup/ 
 chmod 0750 ~/monopse/monopse.sh
 ln -sf ~/monopse/monopse.sh ~/bin/monopse
+PATH=$HOME/bin:$PATH
 
 #
